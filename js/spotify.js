367709fa190a14eec0cc84efa370b91eb1cd1560
gminteer <gminteer@gmail.com>
2020-07-15 17:47:51 -0400

    imported JSO the wrong way

--- js/spotify.js
+++ js/spotify.js
@@ -1,11 +1,10 @@
-/* globals JSO */
-
 // function getAuthToken() {
 //   const clientId = 'dd7f3da7892d4f0b993617370f503172'; // not actually a secret
 //   const redirectUrl = 'https://gminteer.github.io/proj1-test-space/';
 //   const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUrl}`;
 //   location.assign(authUrl);
 // }
+import {JSO} from 'https://unpkg.com/jso/dist/jso.js';
 
 const spotifyClient = new JSO({
   providerID: 'spotify',
