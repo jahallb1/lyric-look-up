7ba8eba102ffd2e8e9b393bc7c0f685993350684
gminteer <gminteer@gmail.com>
2020-07-15 16:37:00 -0400

    push button, receive token?

---
+++ js/script.js
@@ -0,0 +1,3 @@
+import {getAuthToken} from 'spotify.js';
+
+document.querySelector('#req-spotify-token').addEventListener('click', getAuthToken);
--- js/spotify/apikeys.js
+++ js/spotify/apikeys.js
@@ -1,3 +1,4 @@
+// it's safe to reveal OAuth client IDs, keeping this file around for future use.
 const spotifyClientId = 'dd7f3da7892d4f0b993617370f503172';
 
 export {spotifyClientId};
