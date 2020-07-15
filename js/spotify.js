54906d66ddc3504295b62b450b2be1c920a0c9b1
gminteer <gminteer@gmail.com>
2020-07-15 16:46:07 -0400

    cleanup: attempt 2

--- js/spotify.js
+++ js/spotify.js
@@ -1,5 +1,8 @@
-import {spotifyClientId} from 'apikeys.js';
-
 function getAuthToken() {
-  const authUrl = `https://accounts.spotify.com/authorize?client_id=${spotifyClientId}&response_type=token`;
+  const clientId = 'dd7f3da7892d4f0b993617370f503172'; //
+  const redirectUrl = 'https://gminteer.github.io/proj1-test-space';
+  const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUrl}`;
+  location.assign(authUrl);
 }
+
+export {getAuthToken};
--- js/spotify/apikeys.js
+++
@@ -1,4 +0,0 @@
-// it's safe to reveal OAuth client IDs, keeping this file around for future use.
-const spotifyClientId = 'dd7f3da7892d4f0b993617370f503172';
-
-export {spotifyClientId};
