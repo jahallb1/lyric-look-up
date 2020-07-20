76328ecc17cf94001f97c42c8abcea5435a5efcf
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-07-20 17:02:28 -0400

    adjusted code on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,11 +23,11 @@ function getArtistApi(event) {
           for (let i = 0; i < event.detail.artists.length; i++) {
             if (i < event.detail.artists.length - 1) artistsAsString += `${event.detail.artists[i]}, `;
             else artistsAsString += event.detail.artists[i];}
-    let api = apiAddress + apiKey
+    //let api = apiAddress + apiKey
     event.preventDefault();
-    let songTitle = document.getElementById("song").value;
+    //let songTitle = document.getElementById("song").value;
     console.log(songTitle);
-    fetch("https://canarado-lyrics.p.rapidapi.com/lyrics/" + songTitle, {
+    fetch("https://canarado-lyrics.p.rapidapi.com/lyrics/" + artistsAsString + "" + event.song.title, {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "canarado-lyrics.p.rapidapi.com",
@@ -43,12 +43,4 @@ function getArtistApi(event) {
     })
 }
     
-
-      
-
-   
-// clicked feature to search
-// call api from Genius 
-// parse info
-// display data to the site
 
